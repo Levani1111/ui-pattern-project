9beba8a41cf1644def81b81ed823b714dcf97595
Levani Papashvili <papashvililevani@gmail.com>
2020-02-07 13:14:16 -0500

    updeate script.js _7

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+url= "https://api.nasa.gov/planetary/apod?api_key=hkkghHbvnT8Zc9QJAYtX8oSFD72lTC53fUyfMLyS";
+   let accordionMenu =document.querySelector('.accordionMenu');
+  console.log(accordionMenu)
+   //accordionMenu.addEventListener('click',fetchData);
+  function fetchData(){
+     
+    fetch(url)
+     .then(res =>{
+       console.log('success', res)
+       return res.json();
+     })
+     .then(res =>{
+      console.log(res.hdurl)
+      let img = document.createElement("img");
+      img.src = res.hdurl
+      document.getElementsByClassName("panel")[0].appendChild(img);
+    })
+   }
+   document.getElementsByClassName(".accordionMenu").innerHTML =
+accordionMenu + " " + accordionMenu;
+fetchData()
+
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
 
